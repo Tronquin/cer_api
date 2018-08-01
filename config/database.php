5450f5fa682c0eb1f987d193fb9a2a64fbf3ad9b
Ruben Hidalgo <rhidalgo@jacidi.com>
2018-08-01 11:06:24 -0400

    config bd

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
+            'driver' => env('DB_CONNECTION', 'pgsql'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
