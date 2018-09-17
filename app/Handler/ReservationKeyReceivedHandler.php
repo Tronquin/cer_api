50124981ea800e3321c943d94da045aa0f298481
Ruben Hidalgo <rhidalgo@jacidi.com>
2018-09-17 16:07:09 -0400

    llaves cambio variables

--- app/Handler/ReservationKeyReceivedHandler.php
+++ app/Handler/ReservationKeyReceivedHandler.php
@@ -13,15 +13,15 @@ class ReservationKeyReceivedHandler extends BaseHandler {
         $key_delivered = ReservationKey::where('reserva_id', '=', $this->params['data']['reserva_id'])->first();
 
         if (count($key_delivered) > 0){
-            $key_delivered->keys_received = $this->params['data']['keys_received'];
-            $key_delivered->keys_delivered = $key_delivered->keys_delivered - $this->params['data']['keys_received'];
+            $key_delivered->keys_received = $this->params['data']['llaves_recibidas'];
+            $key_delivered->keys_delivered = $key_delivered->keys_delivered - $this->params['data']['llaves_recibidas'];
 
             $response = $key_delivered->save();
         }else{
             $key_delivered = new ReservationKey();
             $key_delivered->reserva_id = $this->params['data']['reserva_id'];
-            $key_delivered->keys_received = $this->params['data']['keys_received'];
-            $key_delivered->keys_delivered = $key_delivered->keys_delivered - $this->params['data']['keys_received'];
+            $key_delivered->keys_received = $this->params['data']['llaves_recibidas'];
+            $key_delivered->keys_delivered = $key_delivered->keys_delivered - $this->params['data']['llaves_recibidas'];
 
             $response = $key_delivered->save();
         }
