a515ab263df4839ef76cda2c8d69ce547f43812f
Emilio Ochoa <eochoa@navicu.com>
2018-12-06 08:15:16 -0400

    Cambiando estructura de lista de idiomas

--- app/Handler/LanguageDeviceHandler.php
+++ app/Handler/LanguageDeviceHandler.php
@@ -31,7 +31,10 @@ class LanguageDeviceHandler extends BaseHandler
 
             foreach ($keyTranslations as $keyTranslation) {
 
-                $temp['translations'][$keyTranslation->deviceType->code][$keyTranslation->key] = $keyTranslation->pivot->translation;
+                $temp['translations'][$keyTranslation->deviceType->code][] = [
+                    'key' => $keyTranslation->key,
+                    'value' => $keyTranslation->pivot->translation
+                ];
             }
 
             $response[] = $temp;
