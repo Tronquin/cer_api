85e2f218f0db12035af3323068a9925cb9ae42a6
Emilio Ochoa <eochoa@navicu.com>
2018-12-11 08:56:49 -0400

    Corrigiendo galerias

--- app/Galery.php
+++ app/Galery.php
@@ -9,63 +9,24 @@ class Galery extends Model
     protected $table = 'galleries';
 
     protected $fillable = [
-        'galeria_id',
-        'type',
-        'parent_galeria_id',
-        'parent_galleries_id',
-        'nombre',
-        'nombre_en',
-        'nombre_fr',
-        'nombre_po',
-        'tipologia_id',
-        'ubicacion_id',
+        'code',
+        'parent_id'
     ];
 
-    // Obtiene las fotos de la galeria
-    public function fotos()
+    public function photos()
     {
-        return $this->hasMany(Photo::class,'galeria_id','galeria_id');
-    }
-
-    //Relacion con las Experiencias
-    public function experiencia()
-    {
-        return $this->hasMany(Experience::class,'galeria_id','galeria_id');
-    }
-
-    //Relacion con la Ubicacion
-    public function ubicacion()
-    {
-        return $this->belongsTo(Location::class,'id');
-    }
-
-    /*
-    public function tipologia()
-    {
-        return $this->belongsTo('App\Tipologia');
-    }*/
-
-    // Relacion para obtener la version web del registro erp de la galeria
-    public function parentErp()
-    {
-        return $this->belongsTo(self::class, 'parent_galeria_id');
-    }
-
-    // Relacion para obtener la version web del registro erp de la galeria
-    public function childErp()
-    {
-        return $this->hasOne(self::class, 'parent_galeria_id');
+        return $this->hasMany(Photo::class, 'galleries_id');
     }
 
     // Relacion para obtener las galerias padres creadas a traves del admin
-    public function parentWeb()
+    public function parent()
     {
-        return $this->belongsTo(self::class, 'parent_galeria_id');
+        return $this->belongsTo(self::class, 'parent_id');
     }
 
     // Relacion para obtener las galerias hijos creadas a traves del admin
-    public function childWeb()
+    public function children()
     {
-        return $this->hasOne(self::class, 'parent_galeria_id');
+        return $this->hasMany(self::class, 'parent_id');
     }
 }
