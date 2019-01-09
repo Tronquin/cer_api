5c047cb57e1bf87895cd1c74db65c04228959e90
Emilio Ochoa <eochoa@navicu.com>
2019-01-09 09:37:43 -0400

    Comentando restriccion por hora

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:600,1',
+            //'throttle:600,1',
             'bindings',
             \Barryvdh\Cors\HandleCors::class,
         ],
