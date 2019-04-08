fced8f0198a3e0f0ef156398fad85aeb05fc036a
Francisco Bejarano <fbejarano@jacidi.com>
2019-04-08 13:57:10 -0400

    migration added, column description table photos

--- database/migrations/2019_04_08_175506_add_column_description_photos.php
+++ database/migrations/2019_04_08_175506_add_column_description_photos.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnTypePhotos extends Migration
+class AddColumnDescriptionPhotos extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddColumnTypePhotos extends Migration
     public function up()
     {
         Schema::table('photos', function (Blueprint $table) {
-            $table->string('type')->nullable();
+            $table->string('description', 50)->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddColumnTypePhotos extends Migration
     public function down()
     {
         Schema::table('photos', function (Blueprint $table) {
-            $table->dropColumn('type');
+            $table->dropColumn('description', 50);
         });
     }
 }
