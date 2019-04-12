4531d4ec01751c01582bfab62111dcb9ca5e47e5
Francisco Bejarano <fbejarano@jacidi.com>
2019-04-12 10:21:56 -0400

    Document Handler fixed

--- app/Handler/ListDocumentHandler.php
+++ app/Handler/ListDocumentHandler.php
@@ -7,7 +7,11 @@ class ListDocumentHandler extends BaseHandler
 {
     public function handle()
     {
-        return Document::latest()->paginate(10);
+        $documents = Document::query()->orderBy('id', 'DESC')->get();
+        return [
+            'res' => count($documents),
+            'data' => $documents
+        ];
     }
 
     public function validationRules()
