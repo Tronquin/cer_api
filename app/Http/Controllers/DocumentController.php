05b17705b64e15ad5007a49ab6f7f793730904ef
Francisco Bejarano <fbejarano@jacidi.com>
2019-04-16 10:44:06 -0400

    DocumentController Fixed

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -34,39 +34,14 @@ class DocumentController extends Controller
         $type = File::mimeType($path);
 
         //Make a response and add a custom header to it
-        $file = convert_from_latin1_to_utf8_recursively($file);
         $response = Response::make($file, 200);
         $response->header('Content-Type', $type);
 
-        return new JsonResponse($response);
+        return $response;
     }
     
-    /**
-     * Encode array from latin1 to utf8 recursively
-     * @param $dat
-     * @return array|string
-     */
-    public static function convert_from_latin1_to_utf8_recursively($dat)
-    {
-        if (is_string($dat)) {
-            return utf8_encode($dat);
-        } elseif (is_array($dat)) {
-            $ret = [];
-            foreach ($dat as $i => $d) {
-                $ret[ $i ] = self::convert_from_latin1_to_utf8_recursively($d);
-            }
- 
-            return $ret;
-        } elseif (is_object($dat)) {
-            foreach ($dat as $i => $d) {
-                $dat->$i = self::convert_from_latin1_to_utf8_recursively($d);
-            }
- 
-            return $dat;
-        } else {
-            return $dat;
-        }
-    }
+
+
     // public function index(Request $request)
     // {
     //     $data = $request->all();
