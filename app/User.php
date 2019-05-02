6676048ccf6ba449aaf52bcf7f97ac6fba6824b6
Francisco Bejarano <fbejarano@jacidi.com>
2019-05-02 09:40:12 -0400

    JWTAuth deleted

--- app/User.php
+++ app/User.php
@@ -3,9 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model implements JWTSubject
+class User extends Model
 {
     protected $table = 'users';
 
@@ -40,28 +39,5 @@ class User extends Model implements JWTSubject
     public function rol()
     {
         return $this->belongsTo(Rol::class, 'rol_id');
-    }
-
-    /**
-     * Get the identifier that will be stored in the subject claim of the JWT.
-     *
-     * @return mixed
-     */
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    /**
-     * Return a key value array, containing any custom claims to be added to the JWT.
-     *
-     * @return array
-     */
-    public function getJWTCustomClaims()
-    {
-        return [
-            'id'              => $this->id,
-            'timestamp'   => $this->created_at->toIso8601String()
-        ];
     }
 }
