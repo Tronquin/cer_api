ecbaea394bd04afb6228aca46f15e47076124fce
developer <rhidalgo@jacidi.com>
2019-05-20 11:15:44 -0400

    checkou email

--- app/Handler/CheckoutHandler.php
+++ app/Handler/CheckoutHandler.php
@@ -5,6 +5,10 @@ namespace App\Handler;
 use App\ReservationPersistence;
 use App\Service\ERPService;
 use App\Status;
+use App\Mail\BaseMail;
+use Illuminate\Support\Facades\Mail;
+use App\Handler\AvailabilityServiceHandler;
+use App\Handler\FindExperiencesHandler;
 
 class CheckoutHandler extends BaseHandler {
 
@@ -25,6 +29,41 @@ class CheckoutHandler extends BaseHandler {
                 $reservation->save();
             }
         }
+        $handler = new FindExperienceHandler(['experiencia_id' => $response['data']['reserva']['experiencia']['id']]);
+        $handler->processHandler();
+        
+        if (!$handler->isSuccess()) {
+            return $response = [
+                'res' => 0,
+                'msg' => 'error para obtener los datos de la experiencia',
+            ];
+        }
+        $experiencia = $handler->getData();
+        $handler = new AvailabilityServiceHandler(['reserva_id' => $this->params['data']['reserva_id'],'funcion' => 'checkin']);
+        $handler->processHandler();
+        
+        if (!$handler->isSuccess()) {
+            return $response = [
+                'res' => 0,
+                'msg' => 'error para obtener los extras contratados',
+            ];
+        }
+        $servicios = $handler->getData();
+        
+        $data['reserva']['extras_contratados'] = $servicios['data']['list']['extras']['extras_contratados'];
+        $total = 0;
+        foreach($data['reserva']['extras_contratados'] as &$extra){
+            $extra['precio']['total'] = round($extra['precio']['total']);
+            $total = $total + ($extra['precio']['total'] * $extra['cantidad']);
+        }
+        $data['experiencia'] = $experiencia['data'];
+        $data['total_reserva'] = $response['data']['reserva']['total_reserva'];
+        $data['reserva']['total_extras_contratados'] = $total;
+        $data['lang'] = $this->params['data']['iso'];
+        
+        $emailInstance = new BaseMail('email.checkout', [$response['data']['reserva']['cliente']['email']], ['data' => $data]);
+
+        Mail::send($emailInstance);
 
         return $response;
     }
