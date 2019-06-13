91d5552b653eca22835be6c13e977cb861191c57
Emilio Ochoa <eochoa@navicu.com>
2019-06-13 11:37:14 -0400

    Correo 48 horas}

--- app/Console/Commands/SendUntilEmails.php
+++ app/Console/Commands/SendUntilEmails.php
@@ -2,8 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Location;
 use App\Reservation;
-use App\Service\ERPService;
 use App\Service\EmailService;
 use Illuminate\Console\Command;
 use CTrans;
@@ -46,24 +46,63 @@ class SendUntilEmails extends Command
         $after48 = new \DateTime('+48 hours');
         
         $reservations = Reservation::query()
+            ->with([
+                'user',
+                'typology',
+                'experience.extras',
+                'apartment.tipologia'
+            ])
             ->where('checkin', '>=', $now)
             ->where('checkin', '<=', $after48)
             ->get();
 
-        $data = [];
-        /* $response = ERPService::completeInfo(); */
-        $data['reserva'] = $response['data'];
-
         foreach ($reservations as $reservation) {
 
+            $location = Location::query()->where('ubicacion_id', $reservation->ubicacion_id)->first();
+
+            foreach ($reservation->experience->extras as $extra) {
+                $extra->extraName = $extra->getFieldTranslation('nombre', $reservation->iso);
+            }
+
+            $data = $reservation->toArray();
+            $data['apartmentName'] = $reservation->apartment->tipologia->getFieldTranslation('nombre', $reservation->iso);
+            $data['dormitorios'] = $reservation->apartment->tipologia->dormitorios()->count();
+            $data['lavabos'] = $reservation->apartment->tipologia->lavabos()->count();
+            $data['experienceName'] = $reservation->experience->getFieldTranslation('name', $reservation->iso);
+            $data['address'] = $location->direccion;
+
             if ($reservation->emailSent && ($reservation->checkin >= $after24 && $reservation->checkin <= $after48)) {
+
                 // Enviar correo de 48 horas
-                EmailService::send('email.48hEmail', CTrans::trans('email.subject.48hs', $reservation->iso),[$data['reserva']['cliente']['email']],['data' => $data]);
+                EmailService::send(
+                    'email.48hEmail',
+                    CTrans::trans('email.subject.48hs', $reservation->iso),
+                    [$reservation->user->email],
+                    [
+                        'data' => [
+                            'reserva' => $data,
+                            'lang' => $reservation->iso
+                        ],
+                    ]
+                );
                 $reservation->emailSent = true;
+
             } elseif ($reservation->emailSent && $reservation->checkin <= $after24) {
+
                 // Enviar correo de 24 horas
-                EmailService::send('email.24hours', CTrans::trans('email.subject.24hs', $reservation->iso),[$data['reserva']['cliente']['email']],['data' => $data]);
+                EmailService::send(
+                    'email.24hours',
+                    CTrans::trans('email.subject.24hs', $reservation->iso),
+                    [$reservation->user->email],
+                    [
+                        'data' => [
+                            'reserva' => $data,
+                            'lang' => $reservation->iso
+                        ],
+                    ]
+                );
                 $reservation->emailSent = true;
+
             } else {
                 continue;
             }
