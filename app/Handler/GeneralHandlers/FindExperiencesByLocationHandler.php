0cd5be36913e222aa88bf0e90316d683c86e38ed
Emilio Ochoa <eochoa@navicu.com>
2019-06-13 17:52:37 -0400

    Temporal para no incluidos

--- app/Handler/GeneralHandlers/FindExperiencesByLocationHandler.php
+++ app/Handler/GeneralHandlers/FindExperiencesByLocationHandler.php
@@ -42,7 +42,8 @@ class FindExperiencesByLocationHandler extends BaseHandler
             }
         }
 
-        $extraNotIncluded = Extra::where('ubicacion_id', $ubicacionId)->where('type', 'erp')->whereIn('id', $extraIdsAll)->with(['child'])->get();
+        //$extraNotIncluded = Extra::where('ubicacion_id', $ubicacionId)->where('type', 'erp')->whereIn('id', $extraIdsAll)->with(['child'])->get();
+        $extraNotIncluded = Extra::query()->whereIn('id', [1, 2, 17, 18])->get();
 
         foreach ($experiencesCollection as $expErp) {
 
@@ -73,14 +74,15 @@ class FindExperiencesByLocationHandler extends BaseHandler
             $noAvailable = [];
             foreach ($extraNotIncluded as $extraNot) {
                 $temp = $extraNot->child ? $extraNot->child->toArray() : $extraNot->toArray();
-                if (!in_array($temp['id'], $extraIds)) {
+                //if (!in_array($temp['id'], $extraIds)) {
                     $temp['front_image'] = $temp['front_image'] ? UrlGenerator::generate('storage.image', ['image' => str_replace('/', '-', $temp['front_image'])]) : null;
                     $temp['icon'] = $temp['icon'] ? UrlGenerator::generate('storage.image', ['image' => str_replace('/', '-', $temp['icon'])]) : null;
                     $temp['fieldTranslations'] = $extraNot->child ? $extraNot->child->fieldTranslations() : $extraNot->fieldTranslations();
-                    $temp['activo'] = (bool)$expErp->extras_experiences_not_included()->where('extras.id', $temp['id'])->count() > 0;
+                    //$temp['activo'] = (bool)$expErp->extras_experiences_not_included()->where('extras.id', $temp['id'])->count() > 0;
+                    $temp['activo'] = true;
 
                     $noAvailable[] = $temp;
-                }
+                //}
             }
 
 
