f45faccc30d1d3cc059a712cd6f9e071d6701af4
Emilio Ochoa <eochoa@navicu.com>
2019-06-17 13:37:17 -0400

    Error de mezcla

--- app/Handler/GeneralHandlers/FindExperiencesByLocationHandler.php
+++ app/Handler/GeneralHandlers/FindExperiencesByLocationHandler.php
@@ -42,15 +42,7 @@ class FindExperiencesByLocationHandler extends BaseHandler
             }
         }
         //Extras Not Included associated to an EXPERIENCE
-        // $extraNotIncluded = Extra::where('ubicacion_id', $ubicacionId)->where('type', 'erp')->whereIn('id', $extraIdsAll)->with(['child'])->get();
-
-<<<<<<< HEAD
-        //All Extras Not Included that are of type 'ERP' associated to that Ubication 
         $extraNotIncluded = Extra::where('ubicacion_id', $ubicacionId)->where('type', 'erp')->with(['child'])->get();
-=======
-        //$extraNotIncluded = Extra::where('ubicacion_id', $ubicacionId)->where('type', 'erp')->whereIn('id', $extraIdsAll)->with(['child'])->get();
-        $extraNotIncluded = Extra::query()->whereIn('id', [1, 2, 17, 18])->get();
->>>>>>> 4221501b14104d20cadc43101a3e33966f3dcd90
 
         foreach ($experiencesCollection as $expErp) {
 
@@ -81,15 +73,14 @@ class FindExperiencesByLocationHandler extends BaseHandler
             $noAvailable = [];
             foreach ($extraNotIncluded as $extraNot) {
                 $temp = $extraNot->child ? $extraNot->child->toArray() : $extraNot->toArray();
-                //if (!in_array($temp['id'], $extraIds)) {
+                if (!in_array($temp['id'], $extraIds)) {
                     $temp['front_image'] = $temp['front_image'] ? UrlGenerator::generate('storage.image', ['image' => str_replace('/', '-', $temp['front_image'])]) : null;
                     $temp['icon'] = $temp['icon'] ? UrlGenerator::generate('storage.image', ['image' => str_replace('/', '-', $temp['icon'])]) : null;
                     $temp['fieldTranslations'] = $extraNot->child ? $extraNot->child->fieldTranslations() : $extraNot->fieldTranslations();
-                    //$temp['activo'] = (bool)$expErp->extras_experiences_not_included()->where('extras.id', $temp['id'])->count() > 0;
-                    $temp['activo'] = true;
+                    $temp['activo'] = (bool)$expErp->extras_experiences_not_included()->where('extras.id', $temp['id'])->count() > 0;
 
                     $noAvailable[] = $temp;
-                //}
+                }
             }
 
 
